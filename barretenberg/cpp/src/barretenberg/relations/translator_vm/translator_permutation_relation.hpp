3fcd46994a30c9f9d8980c5d7dbff5c72ba30573
iakovenkos <sergey.s.yakovenko@gmail.com>
2024-07-16 11:45:19 +0000

    fixed build issues

--- barretenberg/cpp/src/barretenberg/relations/translator_vm/translator_permutation_relation.hpp
+++ barretenberg/cpp/src/barretenberg/relations/translator_vm/translator_permutation_relation.hpp
@@ -14,14 +14,6 @@ template <typename FF_> class TranslatorPermutationRelationImpl {
         3  // left-shiftable polynomial sub-relation WHY?
     };
     /**
-     * @brief Upper bound on total degrees of sub-relations considered as polynomials in witnesses.
-     *
-     */
-    static constexpr std::array<size_t, 2> SUBRELATION_WITNESS_DEGREES{
-        5, // grand product construction sub-relation
-        2  // left-shiftable polynomial sub-relation
-    };
-    /**
      * @brief For ZK-Flavors: The degrees of subrelations considered as polynomials only in witness polynomials,
      * i.e. all selectors and public polynomials are treated as constants.
      *
